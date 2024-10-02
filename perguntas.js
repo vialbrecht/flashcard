76133e687e2102388d7c2a4f8a75f4df85998e87
Vivi <144794590+vialbrecht@users.noreply.github.com>
2024-10-02 07:55:03 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'Matemática',
+    'Quanto é 2 vezes 2',
+    '4'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'Brasília'
+)
+
+criaCartao(
+    'Português',
+    'o certo é, elus ou eles?',
+    'Eles'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil?',
+    'Pedro Alvares Cabral'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 5 vezes 5',
+    '25'
+)
+
+criaCartao(
+    'Química',
+    'qual é o elemento CU',
+    'Cobre'
+)
+
+criaCartao(
+    'Geográfia',
+    'Qual é a capital do PR',
+    'Curitiba'
+)
+
+criaCartao(
+    'Química',
+    'Qual o elemento H',
+    'Hidrogênio'
+)
+
+criaCartao(
+    'Matemática',
+    'Quanto é 9 mais 1',
+    '10'
+)
+
+criaCartao(
+    'História',
+    'Quem foi hitler?',
+    'Um nazista'
+)
